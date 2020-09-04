3dad630182281ca357f37da1eb4519fefd97de60
Amit Shrestha <amitshrestha@Amits-MacBook-Air.local>
2020-09-04 20:16:14 +0545

    remove unused packages

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
