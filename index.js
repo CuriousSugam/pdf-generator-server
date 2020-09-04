07807502f971b49165ed03062ad91c44a9ea83f3
Amit Shrestha <amitshrestha@Amits-MacBook-Air.local>
2020-09-04 20:12:35 +0545

    code cleanup

--- index.js
+++ index.js
@@ -12,84 +12,35 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 
 app.post('/', async (req, res) => {
-	const {
-		content,
-		css,
-		linkHref,
-		options: {
-			fileName,
-			format,
-			headerTemplate,
-			footerTemplate,
-			displayHeaderFooter,
-		},
-	} = req.body.template;
-
-	// fs.writeFile('temp.css', css, (err) => {
-	// 	if (err) throw err;
-	// 	console.log('The css has been saved!');
-	// });
-
-	// fs.writeFile('temp.html', content, (err) => {
-	// 	if (err) throw err;
-	// 	console.log('The html has been saved!');
-	// });
+	const { content, css, linkHref } = req.body.template;
 
 	var options = {
-		headerTemplate: '<p></p>',
-		footerTemplate: '<p></p>',
-		displayHeaderFooter,
-		// margin: {
-		// 	top: '10px',
-		// 	bottom: '30px',
-		// },
 		printBackground: true,
 		path: 'temp.pdf',
 		format: 'A4',
 		landscape: true,
 	};
-	// console.log(content);
-	// var tempContent =
-	// 	// '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" /><style>' +
-	// 	'<style>' +
-	// 	css +
-	// 	'</style>' +
-	// 	// '</style></head><body>' +
-	// 	content;
-	// // '</body></html>';
 
-	const browser = await puppeteer.launch({
-		headless: false,
-	});
+	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 1 });
-	// const tempCss = 'h6{color: red}';
-
 	await page.setContent(`${content}`, {
 		waitUntil: 'networkidle2',
 	});
 
-	// const htmlPath = path.join(`file:${process.cwd()}`, 'temp.html');
-	// console.log('htmlPath ====>', htmlPath);
-	// await page.goto(htmlPath, {
-	// 	waitUntil: 'networkidle2',
-	// });
-
 	if (Array.isArray(css) && css.length > 0) {
-		// await page.addStyleTag({
-		// 	path: path.join(`file:${process.cwd()}`, 'temp.css'),
-		// });
-		// await page.addScriptTag({ url: 'https://d3js.org/d3.v5.min.js' });
 		css.forEach(async (style) => {
 			await page.addStyleTag({
 				content: style,
 			});
 		});
-		// await page.addStyleTag({ content: css });
 	}
-	if (linkHref && linkHref.length) {
+
+	if (Array.isArray(linkHref) && linkHref.length > 0) {
 		linkHref.forEach(async (href) => {
-			await page.addStyleTag({ url: href });
+			await page.addStyleTag({
+				url: href,
+			});
 		});
 	}
 
