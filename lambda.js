44b20630816ea96a2c1d80022ad873185d1c5810
Leapfrog Technology <leapfrogtechnology@Leapfrogs-MacBook-Pro.local>
2020-09-08 18:13:37 +0545

    Added binary mime types in aws serverless express

--- lambda.js
+++ lambda.js
@@ -1,6 +1,10 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
-const server = awsServerlessExpress.createServer(app);
+
+const binaryMimeTypes = [
+	'application/pdf'
+]
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
 exports.handler = (event, context) => {
     awsServerlessExpress.proxy(server, event, context);
